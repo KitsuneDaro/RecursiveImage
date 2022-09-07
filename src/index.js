af8a0592b4e71a696aa0d2b46bb4f11e8531fb1f
KitsuneDaro <106896176+KitsuneDaro@users.noreply.github.com>
2022-09-07 21:45:02 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 $(() => {
-    new Main($('#mainCanvas').get(0),$('#imgFileInput').get(0));
+    new Main($('#mainCanvas').get(0), $('#imgFileInput').get(0), $('#maxTimesInput').get(0));
 });
 
 class Main {
-    constructor(mainCanvas,input) {
+    constructor(mainCanvas, inputImg, inputTimes) {
         this.mainCanvas = mainCanvas;
         this.mainCtx = this.mainCanvas.getContext('2d');
 
-        this.input = input;
+        this.inputImg = inputImg;
 
-        this.input.addEventListener('change', (e) => {
+        this.inputImg.addEventListener('change', (e) => {
             let file = e.target.files;
             let reader = new FileReader();
 
@@ -20,12 +20,17 @@ class Main {
             }
         });
 
+        this.inputTimes = inputTimes;
+
+        this.inputTimes.value = 3;
+        this.inputTimes.min = 0;
+
         this.reimg = null;
         this.interval = null;
     }
 
     readImg(src) {
-        if(this.interval != null) {
+        if (this.interval != null) {
             clearInterval(this.interval);
         }
         let img = new Image();
@@ -47,10 +52,23 @@ class Main {
 
             this.reimg = new RecursiveImage(this.mainCtx, imgData);
 
+            this.intervalTime = 1;
             this.interval = setInterval(() => {
-                this.reimg.divide(2);
-                this.reimg.draw(this.mainCtx);
-            }, 1);
+                let start = performance.now();
+                let i = 0;
+
+                while (i < this.inputTimes.value && (performance.now() - start) < (this.intervalTime / 2)) {
+                    if (!this.reimg.divide(2)) {
+                        clearInterval(this.interval);
+                    }
+                    i += 1;
+                }
+
+                if (i < this.inputTimes.value && this.inputTimes.max === undefined) {
+                    this.inputTimes.value = i + 1;
+                    this.inputTimes.max = i + 1;
+                }
+            }, this.intervalTime);
         }
     }
 }
@@ -59,12 +77,18 @@ class RecursiveImage {
     constructor(ctx, imgData) {
         this.ctx = ctx;
         this.imgData = imgData;
+        this.maxPriority = 0;
+        this.maxSfx = 0;
 
         let range = new Range2D(0, 0, this.imgData.width, this.imgData.height);
-        this.rangePrioritys = [new RangePriority(range, imgData, 0)];
+        this.rangePrioritys = [new RangePriority(range, imgData, 1)];
     }
 
     divide(divisionPart) {
+        if (this.rangePrioritys.length == 0) {
+            return false;
+        }
+
         let rangePriority = this.rangePrioritys.pop();
         let range = rangePriority.range;
         let partWidth = range.width / divisionPart;
@@ -90,6 +114,8 @@ class RecursiveImage {
                 }
             }
         }
+
+        return true;
     }
 
     addRangePriority(rangePriority, left, right) {
@@ -123,11 +149,9 @@ class RecursiveImage {
             fillStyle += rangePriority.average[i] + ',';
         }
 
-        fillStyle += rangePriority.average[3] / 255 + ')';
-
-        this.ctx.fillStyle = fillStyle;
+        this.ctx.fillStyle = fillStyle + '1)';
         this.ctx.fillRect(range.sx, range.sy, range.width, range.height);
-        this.ctx.fillStyle = 'rgba(0,0,0,1)';
+        this.ctx.fillStyle = '#000000';
         this.ctx.beginPath();
         this.ctx.rect(range.sx, range.sy, range.width, range.height);
         this.ctx.stroke();
@@ -146,34 +170,38 @@ class RangePriority {
     }
 
     evalAverage() {
-        this.average = [0, 0, 0, 0];
-        let sum = [0, 0, 0, 0];
+        this.average = [0, 0, 0];
+        let sum = [0, 0, 0];
 
-        for (let x = this.range.sx; x < this.range.ex; x++) {
-            for (let y = this.range.sy; y < this.range.ey; y++) {
-                let sfx = this.getImgDataSfx(x, y);
+        for (let y = this.range.sy; y < this.range.ey; y++) {
+            let sfx = this.getImgDataSfx(this.range.sx, y);
 
-                for (let i = 0; i < 4; i++) {
+            for (let x = this.range.sx; x < this.range.ex; x++) {
+                for (let i = 0; i < 3; i++) {
                     sum[i] += this.imgData.data[sfx + i];
                 }
+
+                sfx += 4;
             }
         }
 
-        for (let i = 0; i < 4; i++) {
-            this.average[i] = sum[i] / this.range.area;
+        for (let i = 0; i < 3; i++) {
+            this.average[i] = Math.floor(sum[i] / this.range.area);
         }
     }
 
     evalVariance() {
-        this.variance = [0, 0, 0, 0];
+        this.variance = [0, 0, 0];
 
-        for (let x = this.range.sx; x < this.range.ex; x++) {
-            for (let y = this.range.sy; y < this.range.ey; y++) {
-                let sfx = this.getImgDataSfx(x, y);
+        for (let y = this.range.sy; y < this.range.ey; y++) {
+            let sfx = this.getImgDataSfx(this.range.sx, y);
 
-                for (let i = 0; i < 4; i++) {
+            for (let x = this.range.sx; x < this.range.ex; x++) {
+                for (let i = 0; i < 3; i++) {
                     this.variance[i] += ((this.imgData.data[sfx + i] - this.average[i]) / 256) ** 2 / this.range.area;
                 }
+
+                sfx += 4;
             }
         }
     }
@@ -181,7 +209,7 @@ class RangePriority {
     evalPriority() {
         this.priority = 0;
 
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 3; i++) {
             this.priority += this.variance[i];
         }
 
